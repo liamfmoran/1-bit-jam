fb1858290627a31ed3cb44b3c93cab73bdb11ef1
Liam Moran <14287694+liamfmoran@users.noreply.github.com>
2026-05-14 20:45:51 -0400

    Added basic ship

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,24 @@
 extends Node2D
+
+@export var zoom_min: float = 0.8
+@export var zoom_max: float = 1.5
+@export var zoom_speed_factor: float = 0.002
+@export var zoom_smoothing: float = 3.0
+
+@onready var truck_cab: RigidBody2D = $TruckCab
+@onready var camera: Camera2D = $Camera2D
+
+var _current_zoom: float = 1.5
+
+
+func _physics_process(delta: float) -> void:
+	_update_camera(delta)
+
+
+func _update_camera(delta: float) -> void:
+	camera.global_position = truck_cab.global_position
+
+	var speed := truck_cab.linear_velocity.length()
+	var target_zoom := clampf(zoom_max - speed * zoom_speed_factor, zoom_min, zoom_max)
+	_current_zoom = lerpf(_current_zoom, target_zoom, zoom_smoothing * delta)
+	camera.zoom = Vector2(_current_zoom, _current_zoom)
--- scenes/ship/truck_cab.gd
+++ scenes/ship/truck_cab.gd
@@ -1,1 +1,51 @@
 extends RigidBody2D
+
+@export var thrust_force: float = 400.0
+@export var rotation_thrust_force: float = 300.0
+
+const ENGINE_RL := Vector2(-10.0, 40.0)
+const ENGINE_RR := Vector2(10.0, 40.0)
+const ENGINE_FL := Vector2(-10.0, -40.0)
+const ENGINE_FR := Vector2(10.0, -40.0)
+const ENGINE_SFR := Vector2(20.0, -25.0)
+const ENGINE_SFL := Vector2(-20.0, -25.0)
+
+@onready var flame_rl: ColorRect = $Flames/FlameRL
+@onready var flame_rr: ColorRect = $Flames/FlameRR
+@onready var flame_fl: ColorRect = $Flames/FlameFL
+@onready var flame_fr: ColorRect = $Flames/FlameFR
+@onready var flame_sfr: ColorRect = $Flames/FlameSFR
+@onready var flame_sfl: ColorRect = $Flames/FlameSFL
+
+
+func _physics_process(_delta: float) -> void:
+	var forward_dir := -transform.y
+	var thrusting_forward := Input.is_action_pressed("thrust_forward")
+	var thrusting_reverse := Input.is_action_pressed("thrust_reverse")
+	var turning_left := Input.is_action_pressed("turn_left")
+	var turning_right := Input.is_action_pressed("turn_right")
+
+	if thrusting_forward:
+		apply_force(forward_dir * thrust_force, ENGINE_RL.rotated(rotation))
+		apply_force(forward_dir * thrust_force, ENGINE_RR.rotated(rotation))
+
+	if thrusting_reverse:
+		apply_force(-forward_dir * thrust_force, ENGINE_FL.rotated(rotation))
+		apply_force(-forward_dir * thrust_force, ENGINE_FR.rotated(rotation))
+
+	if turning_left:
+		apply_force(forward_dir * rotation_thrust_force, ENGINE_SFR.rotated(rotation))
+
+	if turning_right:
+		apply_force(forward_dir * rotation_thrust_force, ENGINE_SFL.rotated(rotation))
+
+	_update_flames(thrusting_forward, thrusting_reverse, turning_left, turning_right)
+
+
+func _update_flames(forward: bool, reverse: bool, left: bool, right: bool) -> void:
+	flame_rl.visible = forward
+	flame_rr.visible = forward
+	flame_fl.visible = reverse
+	flame_fr.visible = reverse
+	flame_sfr.visible = left
+	flame_sfl.visible = right
